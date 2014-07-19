1f12cb599cc68b9c3c26bd1f9f3db66c477fab8e
lakhansamani <lakhan.m.samani@gmail.com>
2014-07-19 12:27:28 +0530

    Refactoring.
    - Adds lib.js which has all library functions
    - Adds index.html
    - Moved library function from index.js to lib.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-console.log("Hello World");
-function add(a,b){
-	return a+b;
-}
-console.log(add(5,4));
 
-function sub(a,b){
-	return a-b;
-}
+console.log("Hello World");
+console.log(add(5,4));
 console.log(sub(9,8));
